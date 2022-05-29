dfe8a74b9e2abf608cc09fe524e030dfd6d7fade
ArtanisCen <1543946889@qq.com>
2022-05-29 22:19:09 +0800

    初始化

---
+++ src/views/Home/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p class="title">推荐歌单</p>
+  </div>
+</template>
+
+<script>
+// 目标: 铺设推荐歌单
+// 1. van-row和van-col来搭建外框布局
+// 2. van-col里内容(van-image和p)
+// 3. 调整间距和属性值
+// 4. 调用封装的api/index.js-推荐歌单api方法
+// 5. 拿到数据保存到data里变量-去上面循环标签
+
+// 目标: 铺设最新音乐
+// 1. van-cell铺设一套标签结构
+// 2. 自定义右侧插槽里小图标, 调整垂直居中
+// 3. api/Home.js和api/index.js-封装导出获取最新音乐接口方法
+// 4. 获取数据循环铺设页面即可
+export default {
+
+}
+</script>
+
+<style scoped>
+/* 标题 */
+.title {
+  padding: 0.266667rem 0.24rem;
+  margin: 0 0 0.24rem 0;
+  background-color: #eee;
+  color: #333;
+  font-size: 15px;
+}
+/* 推荐歌单 - 歌名 */
+.song_name {
+  font-size: 0.346667rem;
+  padding: 0 0.08rem;
+  margin-bottom: 0.266667rem;
+  word-break: break-all;
+  text-overflow: ellipsis;
+  display: -webkit-box; /** 对象作为伸缩盒子模型显示 **/
+  -webkit-box-orient: vertical; /** 设置或检索伸缩盒对象的子元素的排列方式 **/
+  -webkit-line-clamp: 2; /** 显示的行数 **/
+  overflow: hidden; /** 隐藏超出的内容 **/
+}
+
+
+</style>
---
+++ src/views/Search/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+  
+  </div>
+</template>
+
+<script>
+// 目标: 铺设热搜关键字
+// 1. 搜索框van-search组件, 关键词标签和样式
+// 2. 找接口, api/Search.js里定义获取搜索关键词的请求方法
+// 3. 引入到当前页面, 调用接口拿到数据循环铺设页面
+// 4. 点击关键词把值赋予给van-search的v-model变量
+
+// 目标: 铺设搜索结果
+// 1. 找到搜索结果的接口 - api/Search.js定义请求方法
+// 2. 再定义methods里getListFn方法(获取数据)
+// 3. 在点击事件方法里调用getListFn方法拿到搜索结果数据
+// 4. 铺设页面(首页van-cell标签复制过来)
+// 5. 把数据保存到data后, 循环van-cell使用即可(切换歌手字段)
+// 6. 互斥显示搜索结果和热搜关键词
+
+// 目标: 搜索框-显示结果
+// 1. @input给van-search绑定事件
+// 2. @input事件中, 获取搜索结果保存 resultList中
+// 3. 判断如果搜索关键字为空(.length === 0), 数组清空, 阻止网络请求执行(提前return)
+
+// 目标: 加载更多
+// 1. 集成list组件-定义相关的变量(搞懂变量的作用) -监测触底事件
+// 2. 一旦触底, 自动执行onload方法
+// 3. 对page++, 给后台传递offset偏移量参数-请求下一页的数据
+// 4. 把当前数据和下一页新来的数据拼接起来用在当前 页面的数组里
+// (切记) - 加载更多数据后,一定要把loading改成false, 保证下一次还能触发onload方法
+export default {
+ 
+}
+</script>
+
+<style scoped>
+/* 搜索容器的样式 */
+.search_wrap {
+  padding: 0.266667rem;
+}
+
+/*热门搜索文字标题样式 */
+.hot_title {
+  font-size: 0.32rem;
+  color: #666;
+}
+
+/* 热搜词_容器 */
+.hot_name_wrap {
+  margin: 0.266667rem 0;
+}
+
+/* 热搜词_样式 */
+.hot_item {
+  display: inline-block;
+  height: 0.853333rem;
+  margin-right: 0.213333rem;
+  margin-bottom: 0.213333rem;
+  padding: 0 0.373333rem;
+  font-size: 0.373333rem;
+  line-height: 0.853333rem;
+  color: #333;
+  border-color: #d3d4da;
+  border-radius: 0.853333rem;
+  border: 1px solid #d3d4da;
+}
+
+/* 给单元格设置底部边框 */
+.van-cell {
+  border-bottom: 1px solid lightgray;
+}
+</style>
